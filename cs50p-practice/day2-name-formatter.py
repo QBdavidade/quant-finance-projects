2742b3bde23a1c06fb4193b56a204a0ae854c01f
David Ade <davidinioluwaadeola@gmail.com>
2025-12-14 18:19:10 +0100

    Day 2: Name formatter practice
    Add a name formatter that prints user's name in uppercase and title case.

---
+++ cs50p-practice/day2-name-formatter.py
@@ -0,0 +1,8 @@
+# Ask user for their full name (first and last)
+# Print it with no extra spaces
+name = input('What is your full name?').strip()
+
+# Print it in ALL CAPS
+print(name.upper())
+# Print it in Title Case
+print(name.title())
