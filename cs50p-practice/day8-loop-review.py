486507cb10051e2eaead11a95ea40e36cbe6aeaf
David Ade <davidinioluwaadeola@gmail.com>
2025-12-22 23:28:48 +0100

    Add loop examples in day8-loop-review.py
    Day 8: Quick loop review (mini-session)

---
+++ cs50p-practice/day8-loop-review.py
@@ -0,0 +1,17 @@
+print('Example 1: Count to 5')
+for i in range(1,6):
+    print(i)
+
+print('\nExample 2: Print name 3 times')
+for i in range(3):
+    print('David')
+
+print('\nExample 3: Countdown')
+for i in range(5, 0, -1):
+    print(i)
+print('Blast off!')
+
+print('\nExample 4: Loop through names')
+names = ['David', 'Samuel', 'Judah']
+for name in names:
+    print(f' Hello, {name}!')
